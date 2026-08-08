2a8163b604b86260d0be7843a5623e9060b1dcdb
Morteza Saran <m.saran@Sponix.local>
2026-08-08 23:26:24 +0300

    fix static folder casing

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.mount("/static", StaticFiles(directory="static", html=True), name="static")
+app.mount("/static", StaticFiles(directory="Static", html=True), name="static")
 
 Base.metadata.create_all(bind=engine)
 
